3343c8330b83ce364d3704fec13e5053a2b0f43f
up201405840 <up201405840@73877a4c-dc42-4fac-b280-d98f82742ce6>
2015-10-30 21:42:23 +0000

    Criação de ficheiros e já todas classes, falta construtores

---
+++ Contacto.h
@@ -0,0 +1,17 @@
+/*
+ * Contacto.h
+ *
+ *  Created on: 12/10/2015
+ *      Author: Telmo
+ */
+
+#ifndef CONTACTO_H_
+#define CONTACTO_H_
+
+class Contacto {
+public:
+	Contacto();
+	virtual ~Contacto();
+};
+
+#endif /* CONTACTO_H_ */
