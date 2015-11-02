90c66655ed30d118fd923048be7227399ebc16ff
Telmo Barros <T20XX@users.noreply.github.com>
2015-11-02 22:46:13 +0000

    alteracao do anuncio.h  adicao das boleanas para mostrar nome , telemovel e/ou email

---
+++ src/olz.cpp
@@ -0,0 +1,18 @@
+/*
+ * olz.cpp
+ *
+ *  Created on: 02/11/2015
+ *      Author: Jorge
+ */
+
+#include "olz.h"
+
+olz::olz() {
+	// TODO Auto-generated constructor stub
+
+}
+
+olz::~olz() {
+	// TODO Auto-generated destructor stub
+}
+
