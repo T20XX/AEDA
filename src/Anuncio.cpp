745e4d4361cee16b87df393971bf0dd19bf55c9c
up201404572 <up201404572@73877a4c-dc42-4fac-b280-d98f82742ce6>
2015-11-02 23:10:58 +0000

    verAnuncios

--- src/Anuncio.cpp
+++ src/Anuncio.cpp
@@ -12,13 +12,16 @@
 
 int Anuncio::nextID = 0;
 
-Anuncio::Anuncio(int data, string titulo, string categoria, string descricao) {
+Anuncio::Anuncio(int data, string titulo, string categoria, string descricao, bool mostraNome, bool mostraTelemovel, bool mostraEmail) {
 	this->data= data;
 	this->titulo = titulo;
 	this->categoria = categoria;
 	this->descricao = descricao;
 	ID = nextID;
 	nextID++;
+	this->mostraEmail = mostraEmail;
+	this->mostraNome = mostraNome;
+	this->mostraTelemovel = mostraTelemovel;
 }
 
 Anuncio::~Anuncio() {
@@ -27,23 +30,29 @@ Anuncio::~Anuncio() {
 
 //AnuncioVenda
 
-AnuncioVenda::AnuncioVenda(int data, string titulo, string categoria, string descricao, string estado, int preco, bool negociavel) :
-		Anuncio(data, titulo, categoria, descricao) {
+AnuncioVenda::AnuncioVenda(int data, string titulo, string categoria, string descricao, string estado, int preco, bool negociavel, bool mostraNome, bool mostraTelemovel, bool mostraEmail) :
+		Anuncio(data, titulo, categoria, descricao, mostraNome, mostraTelemovel, mostraEmail) {
 	this->preco = preco;
 	this->negociavel = negociavel;
 
-	if(estado == "novo")
-		this->estado = estado;
-	else if(estado == "usando como novo")
-		this->estado = estado;
-	else if(estado == "funcional")
-		this->estado = estado;
-	else if(estado == "para peças")
-		this->estado = estado;
-	else this->estado = "Inválido";
+	if(estado == "novo" || estado == "usando como novo" || estado == "funcional" || estado == "para peças")
+		this->estado == estado;
+	else this->estado == "indisponivel";
 }
 
-AnuncioCompra::AnuncioCompra(int data, string titulo, string categoria, string descricao, string ref) :
-		Anuncio(data, titulo, categoria, descricao) {
+void AnuncioVenda::verAnuncio() const {
+	cout << "ID: " << ID << endl << "Data: " << data << endl << "Estado do Produto: " << estado << endl << "Preço Pretendido: " << preco << endl;
+	if(negociavel)
+		cout << "Possibilidade de Negócio." << endl;
+}
+
+//AnuncioCompra
+
+AnuncioCompra::AnuncioCompra(int data, string titulo, string categoria, string descricao, string ref, bool mostraNome, bool mostraTelemovel, bool mostraEmail) :
+		Anuncio(data, titulo, categoria, descricao, mostraNome, mostraTelemovel, mostraEmail) {
 	this->ref=ref;
+}
+
+void AnuncioCompra::verAnuncio() const{
+
 }
