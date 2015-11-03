b405a346279ebb95a25aaaaff7792505c9cc2aa5
up201405840 <up201405840@73877a4c-dc42-4fac-b280-d98f82742ce6>
2015-11-03 13:38:03 +0000

    main.cpp com menu e selecoes funcionais

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 using namespace std;
 
+void selecao1()
+{
+
+}
+void selecao2()
+{
+
+}
+void selecao3()
+{
+
+}
+
+
+
 int main() {
+	int selecao=-1;
+	while (selecao !=0)
+	{
+		cout << "Menu" << endl;
+		cout << "1. Registar Utilizador" << endl;
+		cout << "2. Gerir Utilizadores" << endl;
+		cout << "3. Gerir Anúncios" << endl;
+		cout << "0. Sair" << endl;
+		while (selecao < 0 || selecao > 3)
+			cin >> selecao;
+	if (selecao == 0)
+		break;
+	else if (selecao == 1)
+		selecao1();
+	else if (selecao == 2)
+		selecao2();
+	else if (selecao == 3)
+		selecao3();
+	selecao = -1;
+	}
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
